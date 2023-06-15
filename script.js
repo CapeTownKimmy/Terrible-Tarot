f3fbb4e62498078e640b59604b403305084290c9
CapeTownKimmy <122791435+CapeTownKimmy@users.noreply.github.com>
2023-06-15 11:31:51 +0100

    Add display message box when selection completed

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const cardContainer = document.getElementById('cardGridContainer');
 const cards = cardContainer.querySelectorAll('.card');
-
+const textContainer = document.getElementById('textContainer');
+const resetBtn = document.getElementById('resetBtn');
+const bodyText = document.querySelector('.bodyText');
+const displayMessage = document.getElementById('messageContainer');
 
 // Show which cards user has selected on a click //
 
@@ -22,7 +25,37 @@ function cardCounter(){
         return   
     } else {
         notSelected.forEach(element => element.classList.add('hideMe'));
+        showBtnAndText();
     }
 }
 
+// Show reset Button - once selection made //
 
+function showBtnAndText() {
+    resetBtn.style.display = 'block';
+    bodyText.innerHTML = 'Your misfortune awaits...';
+    textContainer.style.gap = '22rem'
+    showFortuneBox()
+}
+
+// reset cards on click //
+
+resetBtn.addEventListener('click', refresh);
+
+function refresh(e) {
+    cards.forEach(element => {
+        element.classList.remove('hideMe', 'selected');
+        restoreBtnAndText()
+    })
+}
+function restoreBtnAndText() {
+    resetBtn.style.display = 'none';
+    bodyText.innerHTML = 'Pick 3 cards to know your unfortunate future';
+    textContainer.style.gap = '8rem;';
+}
+
+// Show message box for display of fortune - once selection made //
+
+function showFortuneBox(){
+    displayMessage.style.display = 'block';
+}
