183863f85a0fdae1af43d3f30544c491b8b333bb
CapeTownKimmy <122791435+CapeTownKimmy@users.noreply.github.com>
2023-06-21 13:05:54 +0100

    Edit fortune messages

--- script.js
+++ script.js
@@ -39,67 +39,68 @@ const greeting = 'Welcome my unfortunate friend... you have chosen: ';
 // Fortune Maker Text - Card one, card two and card three//
 const binoculars = [
     'The Binoculars',
-    'Nice from far but far from nice!',
+    'Nice from far but far from nice! Beware the local Karen... she wants to talk to your manager!',
     'Yesterday, all my troubles seemed so far away... Look again, you are using your binoculars backwards!',
-    'Roses are red... my binoculars are blue. Your blinds are open, someones wahtching you!'
+    'Roses are red... my binoculars are blue. Your blinds are open, someones watching you!'
 ];
 const coffeeCup = [
     'The Caffeinated',
-    'More espresso, less depresso!',
-    'Better latte than never!',
-    'Deja Brew. You will recieve the wrong coffee order again!'
+    "More espresso, less depresso! Perk up, old bean. You're mug-nificent!",
+    "Better latte than never! Trouble is brewing and you've ground to a halt!",
+    'Deja Brew. Bean there, done that! Back to the daily grind you go.'
 ]
 const lobster = [
     'The Lobster',
-    "Don't be so shellfish. Would it kill you to crack a smile?",
-    'Nice to sea you. An old friend is back in town.',
-    'When life gives you lemons, order the lobster!'
+    "Don't be so shellfish. Crack a smile, be-claws you're worth it!",
+    'Nice to sea you. An old aquaintance is back in town telling tails about you.',
+    'When life gives you lemons, order the lobster! Wave away the rainy day, kelp is on the way!'
 ];
 const mirror = [
     'The Mirror',
-    'Mirror, Mirror, on the wall. What the hell happened!',
-    "Hold a clock to your mirror, it's time for reflection",
+    'Mirror, Mirror, on the wall. What the hell happened?!',
+    "Hold your watch to your mirror, it's time for reflection. Eye wonder what it is you see?",
     'Objects in the rear view mirror may appear closer than they are... Run! While there is still time!'
 ]
 const skull = [
     'The Skull',
-    'Bone Jour! There is a holiday on the horizon.',
-    "It's a no-brainer, you've lost your mind!",
-    'Bone appetit, enjoy your next meal. It could be your last!'
+    "Bone Jour! Your're just dying for a holiday!",
+    "Keep one eye open for skullduggery, someone's got a bone to pick with you!.",
+    'Bone appetit, enjoy your next meal. It might be your last!'
 ];
 const hotAirBalloon = [
     'The Hot-Air Balloon',
     'Want to hear a balloon joke? Nah, it will probably go over your head!',
     'You are over inflated. Check your ego, amigo!',
-    'You are a basket-case! Get your head out of the clouds!'
+    'You are a basket-case! With you feet on the air and your head on the ground...'
 ]
 const stag = [
     'The Stag',
-    "You are stag-geringly vain, doe not over do it!",
-    'Oh deer, what a year!',
-    "What is love? Baby don't herd me..."
+    "You are stag-geringly vain, doe not over do it! No need to fawn over yourself!",
+    'Oh deer, what a year! Buck up! It might be over soon... just hang on for deer life!',
+    "What is love? Baby don't herd me... Your love life is stuck in a rut."
 ];
 const microscope = [
     'The Microscope',
-    'When life gets blurry, adjust your focus!',
-    "Hocus, pocus. You've now lost your focus!",
+    'When life gets blurry, adjust your focus! You need a recalibration of your identification.',
+    "Hocus, pocus. You've lost your focus! How eye-ronic",
     "It's been scientifically proven that people who have more birthdays live longer. Keep up the good work."
 ]
 const musician = [
     'The Musician',
-    "You're a treble maker!",
+    "Here comes treble! On that note, look sharp because there's no going Bach!",
     'Life without music would b flat! Buy a radio.',
-    'Warning! May contain sax and violins!'
+    'Warning! Your week ahead may contain sax and violins!'
 ];
 const wasp = [
     'The Wasp',
-    'You are nothing more than a wanna-bee!',
+    "You are nothing more than a wanna-bee! That's gotta sting!",
     'To bee or not to bee? Maybe just wing it...',
-    'You are my every-sting!'
+    "You are my every-sting! You're pretty fly for a small fry."
 ]
 
 
-// Show which cards user has selected on a click //
+// Changes the card colour to show which card has been selected on a click  //
+// Selected card can be clicked again to deselect //
 cards.forEach(element => {
     element.addEventListener('click', changeColor)
 })
@@ -111,7 +112,7 @@ function changeColor(e) {
 
 
 
-// Count how many cards have been selected and make submit button visible//
+// Count how many cards have been selected on the third selection 3 cards will turn over to reveal the users choice//
 function cardCounter(){
     let maxClicked = cardContainer.querySelectorAll('.card.selected').length;
     if(maxClicked >= 0 && maxClicked < 3) {
@@ -123,6 +124,7 @@ function cardCounter(){
 
 shuffleArr(cardSet);
 runDraw();
+
 
 // Show reset Button - once selection made //
 function showFlipCards() {
@@ -139,7 +141,7 @@ function showFlipCards() {
 
 
 
-// reset cards on click //
+// reset card deck on click //
 resetBtn.addEventListener('click', refresh);
 
 function refresh(e) {
@@ -170,18 +172,23 @@ function showCards() {
     flipCardContainer.style.visibility = 'visible';
     flipOneAfterAnother()
 }
+
+// flip cards slowly, one by one //
 function flipOneAfterAnother() {
     setTimeout(() => {flipCard[0].style.transform = 'rotateY(180deg)'}, 500);
     setTimeout(() => {flipCard[1].style.transform = 'rotateY(180deg)'}, 1300);
     setTimeout(() => {flipCard[2].style.transform = 'rotateY(180deg)'}, 2100);
 }
+
+
+// Once reset, flip cards back until next round //
 function resetRotate () {
     flipCard[0].style.transform = 'rotateY(0deg)';
     flipCard[1].style.transform = 'rotateY(0deg)';
     flipCard[2].style.transform = 'rotateY(0deg)';
 }
 
-// Shuffle numbers with no duplicate numbers//
+// Shuffle numbers in card deck array to randomise selection with no duplicate numbers//
 
 function shuffleArr(arr) {
     let i = 0;
@@ -197,7 +204,7 @@ function shuffleArr(arr) {
 }
 
 
-// Find index for random numbers //
+// Find index for random numbers of each card and assign correct images and text//
 function runDraw() {
     drawCardOne();
     drawCardTwo();
@@ -206,8 +213,6 @@ function runDraw() {
 
 
 // Card 1//
-
-
 function drawCardOne() {
 
     switch(cardSet[0]) {
@@ -278,8 +283,6 @@ function drawCardOne() {
 
 
 //Card Two//
-
-
 function drawCardTwo() {
 
     switch(cardSet[1]) {
@@ -350,7 +353,6 @@ function drawCardTwo() {
 
 
 // Card Three //
-
 function drawCardThree() {
 
     switch(cardSet[2]) {
@@ -418,9 +420,7 @@ function drawCardThree() {
 }
 
 
-// Build Fortune //
-
-
+// Build Fortune Message from selection//
 function displayFortune() {
     fortuneZero.textContent = greeting + cardOneName + ', ' + cardTwoName + ', ' + cardThreeName + '.';
     fortuneOne.textContent = firstCardFortune;
